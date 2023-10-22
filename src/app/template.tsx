c95e3c46776e1357ffaefc42fe503b95897a8549
Akbar Izza Mahendra Suni <akbarizza09@gmail.com>
2023-10-22 17:26:57 +0700

    fix: build issue

--- src/app/template.tsx
+++ src/app/template.tsx
@@ -1,10 +1,13 @@
 "use client";
-import Header from "@/components/Header";
 import { Box } from "@/components/styledElements";
 import dynamic from "next/dynamic";
 import { ReactNode } from "react";
 
 const ApolloProvider = dynamic(() => import("@/context/ApolloProvider"), {
+  ssr: false,
+});
+
+const Header = dynamic(() => import("@/components/Header"), {
   ssr: false,
 });
 
