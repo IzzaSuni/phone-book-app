6196815d02d8dbac72f7c75a15570e0254dabd68
Akbar Izza Mahendra Suni <akbarizza09@gmail.com>
2023-10-23 04:05:11 +0700

    fix: no ssr wrapper

--- src/app/template.tsx
+++ src/app/template.tsx
@@ -1,4 +1,5 @@
 "use client";
+
 import "react-loading-skeleton/dist/skeleton.css";
 import Header from "@/components/Header";
 import { Box } from "@/components/styledElements";
@@ -7,8 +8,8 @@ import { ReactNode } from "react";
 import Modal from "react-modal";
 import ContactModal from "@/components/ContactModal.tsx";
 import Button from "@/components/Button";
-import useGlobalState, { modalAtom } from "@/hooks/useGlobalState";
-import { useAtom, useAtomValue } from "jotai";
+import { modalAtom } from "@/hooks/useGlobalState";
+import { useAtom } from "jotai";
 import { SnackbarProvider, enqueueSnackbar } from "notistack";
 import useDeviceType from "@/hooks/useDeviceType";
 
@@ -34,46 +35,54 @@ const customModalStyles = {
   },
 };
 
-export default function TemplateRoot({ children }: { children: ReactNode }) {
+function NoSSRTemplate({ children }: { children: ReactNode }) {
   const [showModal, setShoModal] = useAtom(modalAtom);
-  const { isTabletDevice, isMobileDevice } = useDeviceType();
+  const { isMobileDevice, isTabletDevice } = useDeviceType();
 
+  return (
+    <>
+      <Box>
+        <Header />
+        <Box padding={2}>{children}</Box>
+      </Box>
+      <Button
+        datatype="button-show-modal-form-add-contact"
+        position={"fixed"}
+        bottom={isTabletDevice || isMobileDevice ? "8px" : "32px"}
+        right={isTabletDevice || isMobileDevice ? "16px" : "32px"}
+        fontSize={isTabletDevice || isMobileDevice ? 24 : 26}
+        p={2}
+        borderRadius={"100%"}
+        onClick={() => {
+          setShoModal(true);
+          enqueueSnackbar({
+            message: "Please fill the form",
+            variant: "info",
+          });
+        }}
+      >
+        Add Contact
+      </Button>
+      <Modal
+        style={customModalStyles}
+        isOpen={showModal}
+        ariaHideApp={false}
+        onRequestClose={() => setShoModal(false)}
+      >
+        <ContactModal />
+      </Modal>
+    </>
+  );
+}
+
+export default function TemplateRoot({ children }: { children: ReactNode }) {
   return (
     <ApolloProvider>
       <SnackbarProvider
         dense
         anchorOrigin={{ horizontal: "right", vertical: "top" }}
       >
-        <Box>
-          <Header />
-          <Box padding={2}>{children}</Box>
-        </Box>
-        <Button
-          datatype="button-show-modal-form-add-contact"
-          position={"fixed"}
-          bottom={isTabletDevice || isMobileDevice ? "8px" : "32px"}
-          right={isTabletDevice || isMobileDevice ? "16px" : "32px"}
-          fontSize={isTabletDevice || isMobileDevice ? 24 : 26}
-          p={2}
-          borderRadius={"100%"}
-          onClick={() => {
-            setShoModal(true);
-            enqueueSnackbar({
-              message: "Please fill the form",
-              variant: "info",
-            });
-          }}
-        >
-          Add Contact
-        </Button>
-        <Modal
-          style={customModalStyles}
-          isOpen={showModal}
-          ariaHideApp={false}
-          onRequestClose={() => setShoModal(false)}
-        >
-          <ContactModal />
-        </Modal>
+        <NoSSRTemplate>{children}</NoSSRTemplate>
       </SnackbarProvider>
     </ApolloProvider>
   );
